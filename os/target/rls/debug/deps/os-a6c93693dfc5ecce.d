70f56f5e7ea69ca8cda0131897d73f9e94a7cc08
ilya <ilyaandreev2010@gmail.com>
2020-06-28 17:35:02 -0500

    init

--- os/target/rls/debug/deps/os-a6c93693dfc5ecce.d
+++
@@ -1,5 +0,0 @@
-/Users/ilyaandreev/Experimenting/rust-os/os/target/rls/debug/deps/os-a6c93693dfc5ecce.rmeta: src/main.rs
-
-/Users/ilyaandreev/Experimenting/rust-os/os/target/rls/debug/deps/os-a6c93693dfc5ecce.d: src/main.rs
-
-src/main.rs:
