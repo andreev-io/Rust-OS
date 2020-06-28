63dd02c417e70eba2cf0f346010648161b46ede6
ilya <ilyaandreev2010@gmail.com>
2020-06-28 17:33:24 -0500

    init

---
+++ os/target/rls/debug/deps/os-69daaa3220c95fe2.d
@@ -0,0 +1,5 @@
+/Users/ilyaandreev/Experimenting/rust-os/os/target/rls/debug/deps/os-69daaa3220c95fe2.rmeta: src/main.rs
+
+/Users/ilyaandreev/Experimenting/rust-os/os/target/rls/debug/deps/os-69daaa3220c95fe2.d: src/main.rs
+
+src/main.rs:
---
+++ os/target/rls/debug/deps/os-a6c93693dfc5ecce.d
@@ -0,0 +1,5 @@
+/Users/ilyaandreev/Experimenting/rust-os/os/target/rls/debug/deps/os-a6c93693dfc5ecce.rmeta: src/main.rs
+
+/Users/ilyaandreev/Experimenting/rust-os/os/target/rls/debug/deps/os-a6c93693dfc5ecce.d: src/main.rs
+
+src/main.rs:
